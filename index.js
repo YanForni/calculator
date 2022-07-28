fe727882fc14e996c986cf1dd0b92e7ceb167bba
Yan <yanfornimedeiros@gmail.com>
2022-07-28 12:16:07 -0300

    Backup

--- index.js
+++ index.js
@@ -9,10 +9,43 @@ const screen = document.querySelector('.screen')
 const screenH = document.querySelector(".screen__h")
 const grid = document.querySelector('.grid')
 const numpad = document.querySelectorAll('.numpad')
-const colorButton = document.querySelector('.color-button')
+const colorButton = document.querySelectorAll('.color-button')
 const equalButton = document.querySelector('.equal-button')
 
+// themeLight.addEventListener('click', () => {
+//     themeDefault.classList.remove('header__button1--active')
+//     themeDefault.classList.add('header__button1--inactive')
+//     themeLight.classList.remove('header__button2--inactive')
+//     themeLight.classList.add('header__button2--active')
+//     themeDark.classList.remove('header__button3--active')
+//     themeDark.classList.add('header__button3--inactive')
 
+//     header.classList.remove('header--1', 'header--3')
+//     header.classList.add('header--3')
+//     headerButtons.classList.remove('header__buttons--1')
+//     headerButtons.classList.remove('header__buttons--3')
+//     headerButtons.classList.add('header__buttons--2')
+//     body.classList.remove('body--1', 'body--3')
+//     body.classList.add('body--2')
+//     screen.classList.remove('screen--1')
+//     screen.classList.remove('screen--3')
+//     screen.classList.add('screen--2')
+//     screenH.classList.remove('screen__h--1')
+//     screenH.classList.remove('screen__h--3')
+//     screenH.classList.add('screen__h--2')
+//     grid.classList.remove('grid--1')
+//     grid.classList.remove('grid--3')
+//     grid.classList.add('grid--2')
+//     equalButton.classList.remove('equal-button--1')
+//     equalButton.classList.remove('equal-button--3')
+//     equalButton.classList.add('equal-button--2')
+//     colorButton.forEach(button => button.classList.remove('color-button--1'))
+//     colorButton.forEach(button => button.classList.remove('color-button--3'))
+//     colorButton.forEach(button => button.add('color-button--2'))
+//     numpad.forEach(button => button.classList.remove('numpad--1'))
+//     numpad.forEach(button => button.classList.remove('numpad--3'))
+//     numpad.forEach(button => button.classList.add('numpad--2'))
+// });
 
 themeLight.addEventListener('click', () => {
     themeLight.classList.toggle('header__button2--2')
@@ -24,7 +57,7 @@ themeLight.addEventListener('click', () => {
     screenH.classList.toggle('screen__h--2')
     grid.classList.toggle('grid--2')
     equalButton.classList.toggle('equal-button--2')
-    colorButton.classList.toggle('color-button--2') 
+    colorButton.forEach(button => button.classList.toggle('color-button--2'))
     numpad.forEach(button => button.classList.toggle('numpad--2'))
 
 });
@@ -58,6 +91,7 @@ function print(someString){
     //     someString.replaceAll(".",",")
     // }
     console.log(someString);
+    screenH.innerText = `${someString}`
 }
 
 function zero() {
